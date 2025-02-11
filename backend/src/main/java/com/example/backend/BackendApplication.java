9dd5541e563d4cad9faac170ec07703781d56cd7
seulbella <69373121+seulbella@users.noreply.github.com>
2025-02-11 23:50:21 +0900

    Spring Boot와 PostgreSQL 연동오류 해결 - BackendApplication.java 수정

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.example.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // .env 파일 자동 로드
+		System.setProperty("DB_URL", dotenv.get("DB_URL")); // 환경변수 추가
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
